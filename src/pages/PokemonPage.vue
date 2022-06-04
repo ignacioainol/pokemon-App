c49baed789b78f91555c050be4d53c466698aef9
Ignacio Ainol <iggnaxios@gmail.com>
2022-06-04 18:01:21 -0400

    first commit

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Quien es este Pokemon?</h1>
+
+    <PokemonPicture/>
+    <PokemonOptions/>
+  </div>
+</template>
+
+<script>
+import PokemonPicture from '@/components/PokemonPicture.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+
+PokemonPicture
+export default {
+  components: {
+    PokemonPicture,
+    PokemonOptions
+}
+}
+</script>
