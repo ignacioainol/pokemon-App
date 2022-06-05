9dfe13fcb2195ff709d1a04546fc52c58cfd2843
Ignacio Ainol <iggnaxios@gmail.com>
2022-06-05 14:42:14 -0400

    finished design

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -1,5 +1,10 @@
 <template>
-  <h2>Pokemon Picture</h2>
+  <div class="pokemon-container">
+    <div class="card-pokemon">
+      <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="Pokemon">
+      <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg" alt="Pokemon">
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +13,26 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.pokemon-container {
+    height: 200px;
+    width: 100%;
 
+}
+.card-pokemon{
+    position: relative;
+    width: 300px;
+    height: 200px;
+    margin: 0 auto;
+}
+img {
+    width: 200px;
+    margin: auto;
+    display: block;
+    position: absolute;
+    left:19%;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
 </style>
