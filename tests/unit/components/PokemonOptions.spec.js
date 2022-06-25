5f54d2518cbebc6c5d5bd37c7816d1b0c734ae7d
Ignacio Ainol <iggnaxios@gmail.com>
2022-06-25 17:40:03 -0400

    testing in emit

--- tests/unit/components/PokemonOptions.spec.js
+++ tests/unit/components/PokemonOptions.spec.js
@@ -29,9 +29,16 @@ describe("Pokemon Options Components", () => {
     const [li1, li2, li3, li4] = wrapper.findAll('li')
 
     li1.trigger('click')
+    li2.trigger('click')
+    li3.trigger('click')
+    li4.trigger('click')
 
-    console.log(wrapper.emitted('selectionPokemon'))
+    // console.log(wrapper.emitted('selection'))
 
-    // expect(wrapper.emitted('selectionPokemon'))
+    expect(wrapper.emitted('selection').length).toBe(4)
+    expect(wrapper.emitted('selection')[0]).toStrictEqual([1])
+    expect(wrapper.emitted('selection')[1]).toStrictEqual([2])
+    expect(wrapper.emitted('selection')[2]).toStrictEqual([3])
+    expect(wrapper.emitted('selection')[3]).toStrictEqual([4])
   })
 });
